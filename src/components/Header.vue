8c62930970a7dd8663cc0c25810f8e98f8cca513
qiliang1015 <61649777+qiliang1015@users.noreply.github.com>
2022-11-11 02:39:04 +0800

    1 binaries uploaded

---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref } from 'vue';
+
+defineProps({
+  // msg: String
+});
+
+const count = ref(0);
+</script>
+
+<template>
+  <div class="headerBox">
+    <ul class="heaUl">
+      <li>首页</li>
+      <li>Code</li>
+      <li>关于</li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+.heaUl {
+  width: 100%;
+  height: 200px;
+  border: 2px solid #000;
+}
+.heaUl li {
+  list-style: none;
+}
+</style>
